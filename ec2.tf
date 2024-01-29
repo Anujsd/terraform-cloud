a57e49998613d203b5eb9bfee0f74e3e07802d67
asd234 <anujdube0123@gmail.com>
2024-01-29 22:00:53 +0530

    updated

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,18 @@
+terraform {
+  cloud {
+    organization = "tempasd"
+
+    workspaces {
+      name = "devops"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
+  access_key = ""
+  secret_key = ""
+
 }
 
 resource "aws_instance" "myec2" {
