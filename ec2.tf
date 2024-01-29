ebcfe508cdac6950fff2841e7d8f060fb9ba3e5f
asd234 <anujdube0123@gmail.com>
2024-01-29 21:09:25 +0530

    push

---
+++ ec2.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "myec2" {
+  ami           = "ami-0a3c3a20c09d6f377"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
